36996ad532212fd77c59ce3e0badc4556c10c5d8
Unc0dxd <62498953+Unc0dxd@users.noreply.github.com>
2023-08-14 13:46:12 +0300

    .

--- test_main.lua
+++ test_main.lua
@@ -3,7 +3,6 @@ local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-
 local Window = Library:CreateWindow({
     -- Set Center to true if you want the menu to appear in the center
     -- Set AutoShow to true if you want the menu to appear when it is created
@@ -15,6 +14,13 @@ local Window = Library:CreateWindow({
     AutoShow = true,
 })
 
+if Window then
+    -- Close the existing window
+    Library:Unload()
+end
+
+-- Load the new script if the window is closed
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Unc0dxd/Crazy-Chaos-HUB/main/test_main.lua"))()
 -- You do not have to set your tabs & groups up this way, just a prefrence.
 local Tabs = {
     -- Creates a new tab titled Main
@@ -22,4 +28,28 @@ local Tabs = {
     Universal = Window:AddTab('Universal'),
     ['UI Settings'] = Window:AddTab('UI Settings'),
 }
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Groupbox')
+
+-- Get the nicknames of players
+local playerNicknames = {}
+for _, player in ipairs(game.Players:GetPlayers()) do
+    table.insert(playerNicknames, player.Name)
+end
+
+-- Call the AddDropdown function with the player nicknames as values
+LeftGroupBox:AddDropdown('MyMultiDropdown', {
+    Values = playerNicknames,
+    Default = 1, 
+    Multi = true,
+    Text = 'A dropdown',
+    Tooltip = 'This is a tooltip',
+})
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' }) 
+
+Library.ToggleKeybind = Options.MenuKeybind
 
